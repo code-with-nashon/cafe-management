ee78ed3917967f87babebc46ee256a19a91ee78e
fanuel.nashon <fanuel.nashon@yana.dev>
2026-02-08 18:29:42 +0300

    initial commit

---
+++ database/migrations/2026_02_08_152533_create_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('phone_number');
+            $table->integer('total_amount');
+            $table->text('customization_notes')->nullable(); 
+            $table->string('status')->default('pending'); 
+            $table->string('payment_reference')->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app>
+    <div class="max-w-md mx-auto mt-10">
+        <livewire:cafe-item />
+    </div>
+</x-layouts.app>
